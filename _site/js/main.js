5fd503ffa0893a56633c2183ff031841af83a772
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2020-10-02 09:57:06 +0200

    Fixed a CSS issue on mobile

--- _site/js/main.js
+++
@@ -1,7 +0,0 @@
-$(window).on("scroll load resize", () => {
-  if($(window).scrollTop()) {
-    $('.navbar').addClass("top-nav-collapse");
-  }else{
-    $('.navbar').removeClass("top-nav-collapse");
-  }
-});
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(window).on("scroll load resize", () => {
   if($(window).scrollTop()) {
     $('.navbar').addClass("top-nav-collapse");
+    $('.nav-link').addClass("top-nav-collapse");
   }else{
     $('.navbar').removeClass("top-nav-collapse");
+    $('.nav-link').removeClass("top-nav-collapse");
   }
 });
