6f0a76454a9b5b42aea93a30a574cac68369b9a9
Emmanuela Carbé <51440532+emmcarbe@users.noreply.github.com>
2024-07-29 08:52:43 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(window).on("scroll load resize", () => {
-  if($(window).scrollTop()) {
-    $('.navbar').addClass("top-nav-collapse");
-    $('.nav-link').addClass("top-nav-collapse");
-  }else{
-    $('.navbar').removeClass("top-nav-collapse");
-    $('.nav-link').removeClass("top-nav-collapse");
-  }
-});
